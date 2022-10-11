fc7c1e45a431c2f6b06d69100271d24b9a7a431a
CeniusM <cenius.meier@gmail.com>
2022-10-11 09:50:01 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Drawing;
+using Point = System.Drawing.Point;
+using Rectangle = System.Drawing.Rectangle;
+Rectangle bounds = Screen.GetBounds(Point.Empty);
+
+Bitmap GetSreenshot()
+{
+  Bitmap bm = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+  Graphics g = Graphics.FromImage(bm);
+  g.CopyFromScreen(0, 0, 0, 0, bm.Size);
+  return bm;
+}
+
+
+using(Bitmap bitmap = new Bitmap(bounds.Width, bounds.Height))
+{
+    using(Graphics g = Graphics.FromImage(bitmap))
+    {
+         g.CopyFromScreen(Point.Empty, Point.Empty, bounds.Size);
+    }
+    
+    bitmap.Save("test.png");
+}
